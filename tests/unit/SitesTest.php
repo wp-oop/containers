8c027355d0f1466f269065ee4cae34fb86f4ba00
Anton Ukhanev <xedin.unknown@gmail.com>
2021-02-12 16:28:40 +0100

    Fix namespace

--- tests/unit/SitesTest.php
+++ tests/unit/SitesTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace Dhii\Wp\Containers\FuncTest;
+namespace Dhii\Wp\Containers\UnitTest;
 
 use Brain\Monkey\Functions;
 use Dhii\Wp\Containers\Sites as TestSubject;
